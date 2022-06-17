d792cc0e085af363637c6f90cc0a4abfa0e94625
Shivan Sivakumarnan <51132467+ShivanS93@users.noreply.github.com>
2022-06-18 10:39:35 +1200

    updating with bubbletea

--- main.go
+++ main.go
@@ -1,26 +1,78 @@
-package main 
+package main
 
 import (
-        "log"
-        ui "github.com/gizak/termui/v3"
-        "github.com/gizak/termui/v3/widgets"
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"os"
 )
 
+type model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func initialModel() model {
+	return model{
+		choices:  []string{"Buy Carrot", "Buy Celery", "Buy Kohlrabi"},
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "What should we buy at the market?\n\n"
+
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">>"
+		}
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	s += "\nPress q to quit.\n"
+	return s
+}
 func main() {
-        if err := ui.Init(); err != nil {
-                log.Fatalf("failed to initialise termui: %v", err)
-        }
-        defer ui.Close()
-
-        p := widgets.NewParagraph()
-        p.Text = "Hello World!"
-        p.SetRect(0, 0, 50, 10)
-
-        ui.Render(p)
-
-        for e := range  ui.PollEvents() {
-                if e.Type == ui.KeyboardEvent {
-                        break
-                }
-        }
+	p := tea.NewProgram(initialModel())
+	if err := p.Start(); err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
 }
